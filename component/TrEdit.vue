bad8ac434918e70460a517210b8d2b472937aa9a
naykidman <60773318+naykidman@users.noreply.github.com>
2020-02-20 21:54:37 +0900

    Add files via upload

--- component/TrEdit.vue
+++ component/TrEdit.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    <tr>
-      <td>{{ record.product }}</td>
-      <td>{{ record.price }}</td>
-      <td>{{ record.remarks }}</td>
-      <td>
-        <button class="delete-button" @click="deleteIt">delete</button>
-      </td>
-      <td>
-        <button class="edit-button" @click="editIt">edit</button>
-      </td>
-    </tr>
-  </div>
+  <tr>
+    <td></td>
+    <td>{{ record.product }}</td>
+    <td>{{ record.price }}</td>
+    <td>{{ record.remarks }}</td>
+    <td>
+      <button class="delete-button" @click="deleteIt">delete</button>
+      <button class="edit-button" @click="editIt">edit</button>
+    </td>
+  </tr>
 </template>
 
 <script>
